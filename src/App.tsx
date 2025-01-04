769722538607405587ea0f114f7dd12515047f55
Irish Micole Cando <irishmicolecando@gmail.com>
2025-01-05 01:35:32 +0800

    feat: Add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Heading from "./components/Heading";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="font-poppins my-10">
+    <div className="font-poppins my-10 overflow-x-hidden">
       <Navbar />
+      <Heading />
     </div>
   )
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,32 @@
+import illustrationWorking from "../assets/illustration-working.svg"
+
+const Heading = () => {
+  return (
+    <div className="flex flex-row ml-24">
+      <div className="w-1/2 min-h-[600px] flex flex-col justify-center space-y-4">
+          <div className="flex flex-col justify-center">
+            <h1 className="font-extrabold text-7xl leading-tight text-very-dark-blue">
+              More than just shorter links
+            </h1>
+            <p className="text-grayish-violet font-medium mb-5">
+              Build your brand's recognition and get detailed insights on how your links are performing.
+            </p>
+          </div>
+          <button className="bg-teal-400 text-white font-bold rounded-full px-8 py-3 hover:opacity-70 mt-4 w-fit">
+            Get Started
+          </button>
+      </div>
+
+      <div className="flex items-center">
+        <img 
+          src={illustrationWorking} 
+          alt="Person working"
+          className="transform translate-x-16 w-[120%] max-w-none"
+        />
+      </div>
+
+    </div>
+  )
+}
+
+export default Heading
