1bb07719bcf1b98fe8af8b120c24a5ae86205c83
Irish Micole Cando <irishmicolecando@gmail.com>
2025-02-06 08:50:38 +0800

    refactor: Responsiveness of Boost Links section

--- src/components/BoostLinks.tsx
+++ src/components/BoostLinks.tsx
@@ -3,7 +3,7 @@ import GetStartedButton from "./GetStartedButton"
 const BoostLinks = () => {
   return (
     <div className="w-full py-12 flex flex-col items-center h-auto bg-boost-desktop bg-cover bg-dark-violet bg-no-repeat bg-center">
-      <p className="text-white font-bold text-4xl mb-5">Boost your links today!</p>
+      <p className="px-3 text-white font-bold text-center text-3xl md:text-4xl mb-5">Boost your links today!</p>
       <GetStartedButton />
     </div>
   )
